0d82b84b531872f73b3c1553b0a5e6008f3905ca
Seth Kiefer <skiefer@unix5.csc.calpoly.edu>
2023-11-13 13:26:55 -0800

    updated Makefile

--- implementation.cpp
+++ implementation.cpp
@@ -8,9 +8,34 @@
 #include <sys/statvfs.h>
 #include <algorithm>
 
-#include "defs.h"
-
 #include "cpe453fs.h"
+
+#define TYPECODESIZE 4
+#define MODESIZE 2
+#define NLINKSIZE 2
+#define UIDSIZE 4
+#define GIDSIZE 4
+#define RDEVSIZE 4
+#define USERFLAGSSIZE 4
+#define TIMESIZE 4
+#define SIZESIZE 8
+#define ALLBLOCKSSIZE 8
+#define NEXTEXTENTSIZE 4
+
+#define INODESIZE 64
+
+#define SUPERBLOCK 0
+#define INODE 1
+
+#define BLOCKSIZE 4096
+#define INDEX(block) (block*BLOCKSIZE)
+#define ENDBLOCK(base, offset) ((offset - base) >= (BLOCKSIZE-BNUMSIZE))
+
+
+#define LENSIZE 2
+#define BNUMSIZE 4
+
+#define MODEDEX 4
 
 
 /**************************************************************/
@@ -279,6 +304,15 @@ static uint32_t root_node(void *args)
 }
 
 
+
+/**************************************************************/
+/*Read only functions*/
+/**************************************************************/
+int chmod(void *args, uint32_t block_num, mode_t new_mode){
+	struct Args *fs = (struct Args*)args;
+	return pwrite(fs->fd,(void*)(&new_mode), MODESIZE, INDEX(block_num)+MODEDEX);
+	
+}
 
 
 
